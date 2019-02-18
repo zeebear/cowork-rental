a436e2536d48a21679cf2555b31411b28bc90c2d
Matias Coto <maticoto20@gmail.com>
2019-02-18 19:53:19 -0300

    changed migration like 4 times, added office controller and index and trackable user stuff

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,8 +1,13 @@
 class OfficesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def new
+    @office = Office.new
   end
 
   def create
+    @office = Office.new(office_params)
+    @office.user = current_user
+    @office.save
   end
 
   def index
@@ -10,5 +15,9 @@ class OfficesController < ApplicationController
   end
 
   def show
+  end
+
+  def office_params
+    params.require(:office).permit(:name, :price, :workspace_type, :number_of_seats, :coffee, :wifi, :lockers, :kitchen, :location, :coordinates)
   end
 end
