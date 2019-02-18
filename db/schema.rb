83474e0a64f9961853524efe6f1d9244ad215d2f
Matias Coto <maticoto20@gmail.com>
2019-02-18 17:54:57 -0300

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_180654) do
+ActiveRecord::Schema.define(version: 2019_02_18_204650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "office_id"
+    t.bigint "user_id"
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["office_id"], name: "index_bookings_on_office_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "offices", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "coordinates"
+    t.integer "price"
+    t.string "type"
+    t.bigint "user_id"
+    t.integer "number_of_seats"
+    t.boolean "coffee"
+    t.boolean "wifi"
+    t.boolean "lockers"
+    t.boolean "kitchen"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offices_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +65,8 @@ ActiveRecord::Schema.define(version: 2019_02_18_180654) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "offices"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "offices", "users"
+  add_foreign_key "reviews", "bookings"
 end
