9b3e1a6893aa99962ecffafb930b627bdb5421f1
Claire Fahy <zeebear21@gmail.com>
2019-02-18 19:08:55 -0300

    add namespace routes for :owner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'offices#index'
+  namespace :owner do
+    resources :offices, except %i[new create]
+    resources :bookings, except %i[new, create, delete]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
