4da77f503f374ecd2b6318f95ccec23aac875752
Matias Coto <maticoto20@gmail.com>
2019-02-18 18:51:20 -0300

    solved migration problem with booking table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'offices#index'
+  resources :offices, only: [:new, :create, :index, :show] do
+    resources :booking, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
