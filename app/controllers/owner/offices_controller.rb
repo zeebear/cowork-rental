850378438a6da148542d4948782d4463b9fa232a
Claire Fahy <zeebear21@gmail.com>
2019-02-18 19:29:40 -0300

    add methods to offices_controller

--- app/controllers/owner/offices_controller.rb
+++ app/controllers/owner/offices_controller.rb
@@ -1,2 +1,32 @@
 class Owner::OfficesController < ApplicationController
+  before_action :set_office, only: %i[show edit]
+
+  def index
+    @offices = current_user.owned_offices
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @office.update(office_params)
+  end
+
+  def delete
+    @office.destroy
+    redirect_to offices_path
+  end
+
+  private
+
+  def set_office
+    @office = Office.find(params[:id])
+  end
+
+  def office_params
+    params.require(:office).permit(:name, :number_of_seats, :type, :price)
+  end
 end
