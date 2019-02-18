40ece69020b41c4de765cbdfb8f4d0a3208cd176
Claire Fahy <zeebear21@gmail.com>
2019-02-18 17:45:36 -0300

    new model for booking

--- db/migrate/20190218204518_create_bookings.rb
+++ db/migrate/20190218204518_create_bookings.rb
@@ -1,11 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.references :office_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :office, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :status
-      t.date :starting_date
-      t.date :ending_date
+      t.date :start_date
+      t.date :end_date
 
       t.timestamps
     end
