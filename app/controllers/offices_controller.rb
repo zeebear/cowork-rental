a098a9e5976116ddc2bd4270da92e12d0c6e619f
Alfonso Quintana <quintana.f.alfonso@gmail.com>
2019-02-19 17:41:53 -0300

    new office view background created

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,46 +1,20 @@
 class OfficesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-  before_action :set_office, only: [:show, :edit]
+  skip_before_action :authenticate_user!, only: [:index, :new]
+
   def new
     @office = Office.new
     authorize @office
   end
 
   def create
-    @office = Office.new(office_params)
-    authorize @office
-    @office.user = current_user
-    return redirect_to office_path(@office) if @office.save
-
-    render :new
   end
 
   def index
-    @offices = policy_scope(Office).order(created_at: :desc)
+    @offices = policy_scope(Office)
+    # authorize @offices
   end
 
   def show
-  end
 
-  def edit
-  end
-
-  def update
-    authorize @office
-    @office.update(office_params)
-    return redirect_to office_path(@office) if @office.save
-
-    render :edit
-  end
-
-  private
-
-  def office_params
-    params.require(:office).permit(:name, :price, :workspace_type, :number_of_seats, :coffee, :wifi, :lockers, :kitchen, :location, :coordinates)
-  end
-
-  def set_office
-    @office = Office.find(params[:id])
-    authorize @office
   end
 end
