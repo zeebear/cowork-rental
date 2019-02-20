ee6e06b487b9fb5606c6ae0d50debec73476dd02
Matias Coto <maticoto20@gmail.com>
2019-02-20 11:28:10 -0300

    fix bug: seats could be negative

--- app/models/office.rb
+++ app/models/office.rb
@@ -4,4 +4,5 @@ class Office < ApplicationRecord
   has_many :reviews, through: :bookings
   has_many :users, through: :bookings
   validates :user, :name, :price, :workspace_type, :number_of_seats, presence: true
+  validates :number_of_seats, :numericality => { :greater_than_or_equal_to => 0 }
 end
