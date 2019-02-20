725eab95c0c66806d9cc778a7d9217da15d3a13c
Bill Kneapler <wrkneapler@gmail.com>
2019-02-20 12:21:49 -0300

    completed controller for reviews new & create, complted view/review/new, need to add reviews to view for office inex

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,13 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def new?
+    return true
+  end
+  def create?
+    return true
+  end
+end
