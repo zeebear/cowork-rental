fc3b25393ab1dcda05bdcd79c52e342662552ef4
Matias Coto <maticoto20@gmail.com>
2019-02-20 11:24:32 -0300

    just deleted comments

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,9 +1,6 @@
 class Owner::BookingsController < ApplicationController
   before_action :set_booking, only: [:edit, :update]
   def index
-    # @bookings = policy_scope([:booked_offices, Booking]).order(created_at: :desc)
-    # @hola = current_user.booked_offices
-    # raise
     @bookings = policy_scope([:owner, Booking]).owned_bookings(current_user)
   end
 
