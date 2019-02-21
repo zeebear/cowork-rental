7cb78603aeaf04a09e320ed321e1c23c8c3c78cc
Claire Fahy <zeebear21@gmail.com>
2019-02-21 14:08:07 -0300

    center on user button present

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,13 +3,13 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
   const fitMapToMarkers = (map, markers) => {
-    // const bounds = new mapboxgl.LngLatBounds();
-    // markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    // map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-    const sw = new mapboxgl.LngLat(-73.9876, 40.7661);
-    const ne = new mapboxgl.LngLat(-73.9397, 40.8002);
-    const bounds = new mapboxgl.LngLatBounds(sw, ne);
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+    // const sw = new mapboxgl.LngLat(-73.9876, 40.7661);
+    // const ne = new mapboxgl.LngLat(-73.9397, 40.8002);
+    // const bounds = new mapboxgl.LngLatBounds(sw, ne);
+    // map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
   };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
