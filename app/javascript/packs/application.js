88b4df7174e6526ac0eade945d9e2e3c5d9d55b6
maticoto20 <38141472+maticoto20@users.noreply.github.com>
2019-02-21 18:31:06 -0300

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import "../plugins/flatpickr"
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import "../plugins/fly_to"
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
