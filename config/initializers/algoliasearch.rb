36503bcd78843d97d94365a35a392b4852dbfbec
Claire Fahy <zeebear21@gmail.com>
2019-04-08 23:30:44 -0300

    add admin key as env variable

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: 'VY8Q24OXEK', api_key: '3208cecbdacce7e71eb19dd851f50a5f' }
+AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_APP_ID'], api_key: ENV['ALGOLIA_API_KEY'] }
